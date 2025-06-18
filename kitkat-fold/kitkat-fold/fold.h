ba581df47293b9d4e8db1f03e5a814870b7fd6ae
Sagie Tsentsiper <tsents.sagie@gmail.com>
2025-06-18 12:56:13 -0700

    Replace tabs with spaces to avoid inconsistencies in comment display.

--- kitkat-fold/kitkat-fold/fold.h
+++ kitkat-fold/kitkat-fold/fold.h
@@ -4,21 +4,21 @@
 
 /**
  *	Prints the multiplication table, seperating rows by spaces and colums by newlines.
- *  :rows: [IN]	The number of rows in the table.
- *  :cols: [IN]	The number of columns in the table.
+ *  :rows: [IN] The number of rows in the table.
+ *  :cols: [IN] The number of columns in the table.
  */
 void printMultiplicationTable(int rows, int cols);
 /*
  *  Prints the repeated expression (times) times.
- *  :expression: [IN]	The expression to print
- *	:times:		 [IN]	The number of times to print the expression. doesnt print on negetive values.
+ *  :expression: [IN] The expression to print
+ *  :times:      [IN] The number of times to print the expression. doesnt print on negetive values.
  */
 void repeatePrint(std::string expression, int times);
 
 /**
  *  For a given number, returns the length of its representation as string.
- * :number: [IN]	The number to parse.
- * :return:			The length of the number printed representation.
+ * :number: [IN] The number to parse.
+ * :return:      The length of the number printed representation.
  */
 int printedLength(int number);
 
