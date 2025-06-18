0c99acc683b1a9ac2b8edbbc911eb12f0c1b886a
Sagie Tsentsiper <tsents.sagie@gmail.com>
2025-06-18 13:04:44 -0700

    Replace custom print spacing with standard setw.

--- kitkat-fold/kitkat-fold/fold.cpp
+++ kitkat-fold/kitkat-fold/fold.cpp
@@ -1,31 +1,14 @@
 #include "fold.h"
 
 #include <iostream>
+#include <iomanip>  //For std::setw (set width) function.
 
-void repeatePrint(std::string expression, int times) {
-    for (int i = 0; i < times; i++) {
-        std::cout << expression;
-    }
-}
-
-int printedLength(int number) {
-    int len = 0;
-    if (number == 0) { // Special case, so the logic with log10 doesnt fail.
-        return 1;
-    }
-    if (number < 0) { // Plus one for the negetive sign.
-        len++;
-    }
-    len += static_cast<int>(std::log10(static_cast<double>(std::abs(number)))) + 1;
-    return len;
-}
 
 void printMultiplicationTable(int rows, int cols) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols - 1; j++) {
             int targetPrint = (i + 1) * (j + 1);
-            std::cout << targetPrint;
-            repeatePrint(" ", 5 - printedLength(targetPrint));
+            std::cout <<  std::setw(5) << targetPrint;
         }
         std::cout << (i + 1) * cols << std::endl;
     }
--- kitkat-fold/kitkat-fold/fold.h
+++ kitkat-fold/kitkat-fold/fold.h
@@ -8,22 +8,3 @@
  *  :cols: [IN] The number of columns in the table.
  */
 void printMultiplicationTable(int rows, int cols);
-/*
- *  Prints the repeated expression (times) times.
- *  :expression: [IN] The expression to print
- *  :times:      [IN] The number of times to print the expression. doesnt print on negetive values.
- */
-void repeatePrint(std::string expression, int times);
-
-/**
- *  For a given number, returns the length of its representation as string.
- * :number: [IN] The number to parse.
- * :return:      The length of the number printed representation.
- */
-int printedLength(int number);
-
-
-/**
- *  Pretty prints a table of integers.
- */
-void printMultiplicationTable(int rows, int cols);
