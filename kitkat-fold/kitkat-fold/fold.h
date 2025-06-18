b5c20c2c4c9852566ca0251d09a901dfdf4f2a8f
Sagie Tsentsiper <tsents.sagie@gmail.com>
2025-06-18 10:43:26 -0700

    Better docs.

--- kitkat-fold/kitkat-fold/fold.h
+++ kitkat-fold/kitkat-fold/fold.h
@@ -3,17 +3,22 @@
 #include <iostream>
 
 /**
- *  Preety prints a table of integers.
+ *	Prints the multiplication table, seperating rows by spaces and colums by newlines.
+ *  :rows: [IN]	The number of rows in the table.
+ *  :cols: [IN]	The number of columns in the table.
  */
 void printMulTable(int rows, int cols);
 /*
  *  Prints the repeated expression (times) times.
- *  expression:
+ *  :expression: [IN]	The expression to print
+ *	:times:		 [IN]	The number of times to print the expression. doesnt print on negetive values.
  */
 void repeatePrint(std::string expression, int times);
 
 /**
  *  For a given number, returns the length of its representation as string.
+ * :number: [IN]	The number to parse.
+ * :return:			The length of the number printed representation.
  */
 int printedLength(int number);
 
